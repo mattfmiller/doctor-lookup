fea4c30f7b4e6dff1daa302b93f5dfce280ca056
Matt Miller <matt.f.miller@live.com>
2018-04-27 14:11:52 -0700

    Create class to create doctor object for each doctor returned from API.

--- src/main.js
+++ src/main.js
@@ -3,32 +3,26 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { APICall } from './scripts.js'
+import { Doctor } from './doctor.js'
 
 let displayResults = function(result) {
   if (result.data.length === 0) {
     $("#doctor-info").append("There were no matching doctors for your search.");
   } else {
     for (var i = 0; i < result.data.length; i++) {
-      $("#doctor-info").append(`<br><br><h5>Dr. ${result.data[i].profile.first_name} ${result.data[i].profile.last_name}, ${result.data[i].profile.title}</h5>
-      <p>${result.data[i].practices[0].visit_address.street}<br>
-      ${result.data[i].practices[0].visit_address.street2}<br>
-      ${result.data[i].practices[0].visit_address.city}, ${result.data[i].practices[0].visit_address.state} ${result.data[i].practices[0].visit_address.zip}</p>
-      <p>Phone: ${result.data[i].practices[0].phones[0].number}</p>
-      `);
-      if (result.data[i].practices[0].website !== undefined) {
-        $("#doctor-info").append(`<p>Website: ${result.data[i].practices[0].website}<p>`);
-      }
-      if (result.data[i].practices[0].accepts_new_patients === true) {
-        $("#doctor-info").append("Accepting New Patients: Yes");
-      } else {
-        $("#doctor-info").append("Accepting New Patients: No");
-      }
+      let doctor = new Doctor(result.data[i]);
+      $("#doctor-info").append(`<strong>${doctor.name}</strong><br>
+      ${doctor.address}<br>
+      ${doctor.phone}<br>
+      ${doctor.website}<br>
+      ${doctor.accepting}<br>`
+      )
     }
   }
 }
 
 let displayError = function (error) {
-  $("#doctor-info").append(`There was an error while processig your request: ${error.message}`);
+  $("#doctor-info").append(`There was an error while processinsg your request: ${error.message}`);
 }
 
 $(document).ready(function(){
--- src/scripts.js
+++ src/scripts.js
@@ -19,4 +19,4 @@ class APICall {
   }
 }
 
-export { APICall }
+export { APICall };
