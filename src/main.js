2e4f07169b88390693e4beb7951f9eea100c8948
Matt Miller <matt.f.miller@live.com>
2018-04-27 11:03:21 -0700

    Add loop to display function to return all matching doctors

--- src/main.js
+++ src/main.js
@@ -5,16 +5,18 @@ import './styles.css';
 import { APICall } from './scripts.js'
 
 let displayResults = function(result) {
-  $("#doctor-info").append(`<br><br><h5>Dr. ${result.data[0].profile.first_name} ${result.data[0].profile.last_name}, ${result.data[0].profile.title}</h5><br>
-  <p>${result.data[0].practices[0].visit_address.street}<br>
-  ${result.data[0].practices[0].visit_address.street2}<br>
-  ${result.data[0].practices[0].visit_address.city}, ${result.data[0].practices[0].visit_address.state} ${result.data[0].practices[0].visit_address.zip}</p>
-  <p>Phone: ${result.data[0].practices[0].phones[0].number}</p>
-  `);
-  if (result.data[0].practices[0].accepts_new_patients === true) {
-    $("#doctor-info").append("Accepting New Patients: Yes")
-  } else {
-    $("#doctor-info").append("Accepting New Patients: No")
+  for (var i = 0; i < result.data.length; i++) {
+    $("#doctor-info").append(`<br><br><h5>Dr. ${result.data[i].profile.first_name} ${result.data[i].profile.last_name}, ${result.data[i].profile.title}</h5>
+    <p>${result.data[i].practices[0].visit_address.street}<br>
+    ${result.data[i].practices[0].visit_address.street2}<br>
+    ${result.data[i].practices[0].visit_address.city}, ${result.data[i].practices[0].visit_address.state} ${result.data[i].practices[0].visit_address.zip}</p>
+    <p>Phone: ${result.data[i].practices[0].phones[0].number}</p>
+    `);
+    if (result.data[i].practices[0].accepts_new_patients === true) {
+      $("#doctor-info").append("Accepting New Patients: Yes")
+    } else {
+      $("#doctor-info").append("Accepting New Patients: No")
+    }
   }
 }
 
