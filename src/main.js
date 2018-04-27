f48b5026d1bfa48d7dd408910e18843b039d271f
Matt Miller <matt.f.miller@live.com>
2018-04-27 10:57:58 -0700

    Add address, phone and accepting new patients to display function

--- src/main.js
+++ src/main.js
@@ -5,7 +5,17 @@ import './styles.css';
 import { APICall } from './scripts.js'
 
 let displayResults = function(result) {
-  $("#doctor-info").append(`Doctor: Dr.  ${result.data[0].profile.first_name} ${result.data[0].profile.last_name}, ${result.data[0].profile.title}`);
+  $("#doctor-info").append(`<br><br><h5>Dr. ${result.data[0].profile.first_name} ${result.data[0].profile.last_name}, ${result.data[0].profile.title}</h5><br>
+  <p>${result.data[0].practices[0].visit_address.street}<br>
+  ${result.data[0].practices[0].visit_address.street2}<br>
+  ${result.data[0].practices[0].visit_address.city}, ${result.data[0].practices[0].visit_address.state} ${result.data[0].practices[0].visit_address.zip}</p>
+  <p>Phone: ${result.data[0].practices[0].phones[0].number}</p>
+  `);
+  if (result.data[0].practices[0].accepts_new_patients === true) {
+    $("#doctor-info").append("Accepting New Patients: Yes")
+  } else {
+    $("#doctor-info").append("Accepting New Patients: No")
+  }
 }
 
 let displayError = function () {
