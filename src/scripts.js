665f41796944460c748bc4bb627e07002a3f7c12
Matt Miller <matt.f.miller@live.com>
2018-04-27 15:36:34 -0700

    Add specialty API request.

--- src/scripts.js
+++ src/scripts.js
@@ -17,6 +17,24 @@ class APICall {
     });
     return promise;
   }
+
+  specialtyApiCall() {
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let error;
+      let url =`https://api.betterdoctor.com/2016-03-01/specialties?user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url);
+      request.send();
+    });
+    return promise;
+  }
 }
 
 export { APICall };
