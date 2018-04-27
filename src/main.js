b7d1e03fc3276ee56d99e249a4113235f2b14be6
Matt Miller <matt.f.miller@live.com>
2018-04-27 11:38:42 -0700

    Add branching to notify user when query returns no results.

--- src/main.js
+++ src/main.js
@@ -5,17 +5,21 @@ import './styles.css';
 import { APICall } from './scripts.js'
 
 let displayResults = function(result) {
-  for (var i = 0; i < result.data.length; i++) {
-    $("#doctor-info").append(`<br><br><h5>Dr. ${result.data[i].profile.first_name} ${result.data[i].profile.last_name}, ${result.data[i].profile.title}</h5>
-    <p>${result.data[i].practices[0].visit_address.street}<br>
-    ${result.data[i].practices[0].visit_address.street2}<br>
-    ${result.data[i].practices[0].visit_address.city}, ${result.data[i].practices[0].visit_address.state} ${result.data[i].practices[0].visit_address.zip}</p>
-    <p>Phone: ${result.data[i].practices[0].phones[0].number}</p>
-    `);
-    if (result.data[i].practices[0].accepts_new_patients === true) {
-      $("#doctor-info").append("Accepting New Patients: Yes");
-    } else {
-      $("#doctor-info").append("Accepting New Patients: No");
+  if (result.data.length === 0) {
+    $("#doctor-info").append("There were no matching doctors for your search.");
+  } else {
+    for (var i = 0; i < result.data.length; i++) {
+      $("#doctor-info").append(`<br><br><h5>Dr. ${result.data[i].profile.first_name} ${result.data[i].profile.last_name}, ${result.data[i].profile.title}</h5>
+      <p>${result.data[i].practices[0].visit_address.street}<br>
+      ${result.data[i].practices[0].visit_address.street2}<br>
+      ${result.data[i].practices[0].visit_address.city}, ${result.data[i].practices[0].visit_address.state} ${result.data[i].practices[0].visit_address.zip}</p>
+      <p>Phone: ${result.data[i].practices[0].phones[0].number}</p>
+      `);
+      if (result.data[i].practices[0].accepts_new_patients === true) {
+        $("#doctor-info").append("Accepting New Patients: Yes");
+      } else {
+        $("#doctor-info").append("Accepting New Patients: No");
+      }
     }
   }
 }
