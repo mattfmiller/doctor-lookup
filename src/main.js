f266566c2f5427f2e2e9659992680730f2525bda
Matt Miller <matt.f.miller@live.com>
2018-04-27 10:23:42 -0700

    Add display function to show doctor name.

--- src/main.js
+++ src/main.js
@@ -4,24 +4,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { APICall } from './scripts.js'
 
-let displayResults = function() {
-
+let displayResults = function(result) {
+  $("#doctor-info").append(`Doctor: Dr.  ${result.data[0].profile.first_name} ${result.data[0].profile.last_name}, ${result.data[0].profile.title}`);
 }
 
 let displayError = function () {
-  
+
 }
 
 $(document).ready(function(){
-  let seach = new ApiCall();
-  let medicalIssue = $("#medical-issue").val();
-  let doctorName = $("#doctor-name").val();
+  $("#button").click(function(){
+    let search = new APICall();
+    let medicalIssue = $("#medical-issue").val();
+    let doctorName = $("#doctor-name").val();
 
-  search.betterDoctorApiCall(medicalIssue, doctorName).then(function(response) {
-    let result = JSON.parse(response);
-    displayResults();
-  }, function() {
-    displayError(error);
+    search.betterDoctorApiCall(medicalIssue, doctorName).then(function(response) {
+      let result = JSON.parse(response);
+      console.log(result);
+      displayResults(result);
+    }, function() {
+      displayError();
+    });
   });
-
 });
